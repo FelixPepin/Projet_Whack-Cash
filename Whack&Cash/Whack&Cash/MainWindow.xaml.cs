25df4f8395bb3350c9228072e4943198b2fd2337
FelixPepin <fepep002@edu.cegepgarneau.ca>
2025-09-26 15:38:56 -0400

    Add files via upload

---
+++ Whack&Cash/Whack&Cash/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Whack_Cash
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_jouer_Click(object sender, RoutedEventArgs e)
+        {
+            FenetreJouer fenetreJouer = new FenetreJouer();
+            fenetreJouer.Show();
+            this.Close();
+        }
+    }
+}
